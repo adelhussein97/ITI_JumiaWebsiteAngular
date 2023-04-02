fe2ac59e6d4799c718f2ace524a419b54ddb0c02
adelhussein97 <adelhussein1579@gmail.com>
2023-04-02 20:51:20 +0200

    Update Router Link and Merge All Pull Requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Components/footer/footer.component';
 // import { ProductComponent } from './Components/product/product.component';
 
 import { HeaderComponent } from './Components/header/header.component';
-import { FormsModule } from '@angular/forms';
 import { MainComponent } from './Components/main/main.component';
 
 
@@ -15,6 +13,7 @@ import { MainComponent } from './Components/main/main.component';
 import { HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './Components/home/home.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -26,7 +25,7 @@ import { NotFoundComponent } from './Components/not-found/not-found.component';
     FooterComponent,
     HomeComponent,
     NotFoundComponent,
-    
+
 
 
 
