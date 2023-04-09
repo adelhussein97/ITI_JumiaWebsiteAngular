8c231c8d54978601a9c2f1382c5c8127e09fe2ca
adelhussein97 <adelhussein1579@gmail.com>
2023-04-09 22:09:20 +0200

    update conflicts

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -7,33 +7,32 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-
-export class HeaderComponent implements OnInit{
-  constructor(public _authServices: AuthServicesService,
-   private translate: TranslateService
-  )
-   {
-    this._authServices.cartSubject.subscribe((data)=>{
-      this.cartItem=data
+export class HeaderComponent implements OnInit {
+  cartItem: number = 0;
+  constructor(
+    public _authServices: AuthServicesService,
+    private translate: TranslateService
+  ) {
+    this._authServices.cartSubject.subscribe((data) => {
+      this.cartItem = data;
     });
-   }
+  }
   ngOnInit(): void {
     this.cartItemFunc();
     this.onLangchange();
+  }
+  cartItemFunc() {
+    if (localStorage.getItem('localCart') != null) {
+      var cartCount = JSON.parse(localStorage.getItem('localCart')!);
+      this.cartItem = cartCount.length;
+    }
   }
   onLangchange() {
     this.translate.onLangChange.subscribe((res) => {
       console.log(`${res}`);
     });
-  cartItem:number=0;
+  }
   changeLang(event: any) {
     this.translate.setDefaultLang(event.target.value);
-  cartItemFunc(){
-if(localStorage.getItem('localCart')  !=null){
-  var cartCount =JSON.parse(localStorage.getItem('localCart')!);
-  this.cartItem=cartCount.length;
-}
   }
-
-
 }
