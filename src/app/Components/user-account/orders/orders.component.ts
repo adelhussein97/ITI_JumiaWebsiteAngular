0ffc653c6f6fd4a76849a1cbe689ca9211ba61b7
Ahmed Sameh <ahmeds14@protonmail.ch>
2023-04-14 06:34:21 +0200

    LastandList

--- src/app/Components/user-account/orders/orders.component.ts
+++ src/app/Components/user-account/orders/orders.component.ts
@@ -5,20 +5,18 @@ import { CartApiService } from 'src/app/services/cart-api.service';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
   carts: Icart[] = [];
-  userId:any =""
-  constructor (private cartsApi: CartApiService,private location:Location)
-  {
-  }
-  ngOnInit (): void
-  {
-    this.userId = localStorage.getItem( "UserId" )?.toString();
+  userId: any = '';
+  constructor(private cartsApi: CartApiService) {}
+  ngOnInit(): void {
+    this.userId = localStorage.getItem('UserId')?.toString();
 
-    this.cartsApi.GetCartsByUserId(this.userId).subscribe(data => {  this.carts=data   })
-
+    this.cartsApi.GetCartsByUserId(this.userId).subscribe((data) => {
+      this.carts = data;
+    });
   }
   // orders = [
   //   { id: '123', date: '2022-04-10', total: 100.00, status: 'Shipped' },
@@ -26,12 +24,11 @@ export class OrdersComponent implements OnInit {
   //   { id: '789', date: '2022-04-08', total: 75.00, status: 'Pending' }
   // ];
 
-  viewOrderDetails(order:any) {
+  viewOrderDetails(order: any) {
     // TODO: Implement logic to view the details of an order
   }
 
-  editOrder(order:any) {
+  editOrder(order: any) {
     // TODO: Implement logic to edit an order
   }
-
 }
--- src/app/Components/user-account/user-account.module.ts
+++ src/app/Components/user-account/user-account.module.ts
@@ -9,19 +9,31 @@ import { OrdersComponent } from './orders/orders.component';
 import { PaymentmethodsComponent } from './paymentmethods/paymentmethods.component';
 import { AddressbookComponent } from './addressbook/addressbook.component';
 
-const routes: Routes =[
-  {path:'',redirectTo:'/profile/userprofile',pathMatch:'full'},
-  {path:'userprofile',component:UserProfileComponent ,title:'userprofile'},
-  {path:'editprofile',component:EditprofileComponent ,title:'editprofile'},
-  {path:'changepassword',component:ChangepasswordComponent ,title:'changepassword'},
-  {path:'orders',component:OrdersComponent ,title:'orders'},
-  {path:'payment',component:PaymentmethodsComponent ,title:'payment'},
-  {path:'addressbook',component:AddressbookComponent ,title:'addressbook'},
-
-
-]
-
-
+const routes: Routes = [
+  { path: '', redirectTo: '/profile/userprofile', pathMatch: 'full' },
+  {
+    path: 'userprofile',
+    component: UserProfileComponent,
+    title: 'userprofile',
+  },
+  {
+    path: 'editprofile',
+    component: EditprofileComponent,
+    title: 'editprofile',
+  },
+  {
+    path: 'changepassword',
+    component: ChangepasswordComponent,
+    title: 'changepassword',
+  },
+  { path: 'MyOrders', component: OrdersComponent, title: 'AllOrders' },
+  { path: 'payment', component: PaymentmethodsComponent, title: 'payment' },
+  {
+    path: 'addressbook',
+    component: AddressbookComponent,
+    title: 'addressbook',
+  },
+];
 
 @NgModule({
   declarations: [
@@ -30,12 +42,8 @@ const routes: Routes =[
     ChangepasswordComponent,
     OrdersComponent,
     PaymentmethodsComponent,
-    AddressbookComponent
+    AddressbookComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild(routes),
-  ]
+  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
 })
-export class UserAccountModule { }
+export class UserAccountModule {}
