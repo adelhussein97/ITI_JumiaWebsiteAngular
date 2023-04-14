598758c9fc072661a0a9c3f22f7f2a56971ece3b
KaramOsman <karam.Etman2998@gmail.com>
2023-04-14 17:12:14 +0200

    KaramFromMain

--- src/app/Components/contact-module/contact.module.ts
+++ src/app/Components/contact-module/contact.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
 
 const routes : Routes = [
@@ -16,6 +18,7 @@ const routes : Routes = [
   ],
   imports: [
     CommonModule,
+    TranslateModule,
     RouterModule.forChild(routes),
   ]
 })
